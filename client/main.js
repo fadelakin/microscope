f7c4fca27575e8d2857048b13f6a8b9b7c0a5325
Fisher Adelakin <temidayoadelakin@gmail.com>
2013-12-20 20:33:54 -0500

    Upgraded core to Meteor 0.7.0.1

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
-postsHandle = Meteor.subscribeWithPagination('newPosts', 10);
+newPostsHandle = Meteor.subscribeWithPagination('newPosts', 10);
 topPostsHandle = Meteor.subscribeWithPagination('topPosts', 10);
 
 Meteor.autorun(function() {
